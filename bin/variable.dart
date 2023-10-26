90ee4869b0a3a2404fd4fca620bb49d1d3da4fc2
Biniljacobpro <biniljacob274@gmail.com>
2023-10-26 15:20:06 +0530

    commit

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,3 +1,13 @@
 void main() {
-
+ String name="Binil jacob";
+ int age= 22;
+ String email="biniljacob@gmail.com";
+ double cgpa= 8;
+ int phone=5654345654;
+ print("My name is $name");
+ print("I am $age years old");
+ print("My cgpa is $cgpa");
+ print("My phone number is $phone");
+ int a=10,b=20;
+ print("total sum = ${a+b}");
 }
