a65da9c008dbc27b104b42215409c42598493806
Biniljacobpro <biniljacob274@gmail.com>
2023-10-26 14:05:24 +0530

    first commit

---
+++ bin/firstprogram.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("My name is Binil jacob");
+  print("I am 22 years old");
+  print("I am studying in luminar technolab");
+  print("My email id is biniljacob274@gmail.com");
+  print("My phone number is 986756345");
+  print(7+5);
+  print(45*2);
+}
