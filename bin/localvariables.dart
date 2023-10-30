58172b74c75ec9a3b2cd8f585a542f8c2f89d280
Biniljacobpro <biniljacob274@gmail.com>
2023-10-30 13:52:16 +0530

    commit

---
+++ bin/localvariables.dart
@@ -0,0 +1,20 @@
+void main(){
+  int x=10;
+  print("x = $x");
+  add();
+  sub();
+  multi();
+}
+void add(){
+  int a=100,b=230;
+  print('sum = ${a+b}');
+}
+void sub(){
+  int a=300, b=67;
+  print('Anser =${a-b}');
+}
+void multi() {
+  int a = 45,
+      b = 4;
+  print('product =${a * b}');
+}
