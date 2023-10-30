1483be6bf5ed2e52f22e9c8cbc7340d746ffeda4
Biniljacobpro <biniljacob274@gmail.com>
2023-10-30 14:04:47 +0530

    commit

--- bin/localvariables.dart
+++ bin/localvariables.dart
@@ -1,20 +1,34 @@
 void main(){
-  int x=10;
+  int x=10;///local varable
   print("x = $x");
   add();
   sub();
   multi();
+  Demo ob= Demo(); ///object creation
+  ob.addition();
+  ob.subtraction();
 }
-void add(){
+void add(){    ///user defined fuction
   int a=100,b=230;
   print('sum = ${a+b}');
 }
 void sub(){
   int a=300, b=67;
-  print('Anser =${a-b}');
+  print('Answer =${a-b}');
 }
 void multi() {
   int a = 45,
       b = 4;
   print('product =${a * b}');
+}
+class Demo{
+  int a=100,b=200;
+  void addition(){ ///user defined method
+    int c= 50;
+    print('\nInside class:');
+    print('Sum =${a+b+c}');
+  }
+  void subtraction(){
+    print('Answer=${b-a}');
+  }
 }
