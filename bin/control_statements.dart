6ce8d2851f4b2c2c77b0627079f0f0e4f60fcb4b
Biniljacobpro <biniljacob274@gmail.com>
2023-11-01 14:43:47 +0530

    commit

--- bin/control_statements.dart
+++ bin/control_statements.dart
@@ -1,8 +1,68 @@
 void main(){
-  int age=2;
+  ///String name ="binil";
 
-  if (age>=18){
-    print("Welcome to voting");
+
+ /* if (name=="binil"){
+    print("Welcome  to Home page");
+  }*/
+
+
+ /* if(name=="binil"){
+    print("welcome to Home page");
+  }else{
+    print("Wrong name entered");
+  }*/
+/*
+  String username ="admin";
+  String password ="abc123";
+  int  otp =1234;
+
+  if(username =="admin" && password == "abc123")
+  {
+    print("Email password login success");
+    if(otp==1234)
+    {
+      print("Otp verified");
+    }
+    else{
+      print("Wrong otp");
+    }
+  }
+  else {
+    print("Invalid password or Username");
+  }
+*/
+
+ /* String size = "l";
+
+  if(size == 's'){
+    print("Shirt size is S");
+  }else if(size == 'm'){
+    print("Shirt size is M");
+  }else if(size == 'l'){
+    print("Shirt size is L");
+  }else if(size == 'xl'){
+    print("Shirt size is XL");
+  }else{
+    print("Your size is not available");
+  }  */
+
+ /* int size=5;
+  switch(size){
+    case 3:print("Shoe size is 3");
+    case 4:print("Shoe size is 4");
+    case 5:print("Shoe size is 5");
+    default:print("No match ");
+  }  */
+
+  int a=190,b=80,c=130;
+  if(a>b && a>c) {
+    print("$a is Largest");
+  }
+  else if(b>a && b>c){
+    print("$b is Largest");
+  }else{
+    print("$c is largest");
   }
 
   print("Thank you");
